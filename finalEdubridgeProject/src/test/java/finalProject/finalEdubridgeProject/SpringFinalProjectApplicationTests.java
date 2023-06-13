66b33fb01c68f45ea8c2e78a2772761affb4f8ea
Bhagyashri Tamrakar <86471670+BST82@users.noreply.github.com>
2023-06-13 11:47:19 +0530

    BackendProjectPart

---
+++ finalEdubridgeProject/src/test/java/finalProject/finalEdubridgeProject/SpringFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package finalProject.finalEdubridgeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
